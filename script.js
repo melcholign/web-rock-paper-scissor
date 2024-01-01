2c40a2fdb94f20b539cbb5f95d5e7257993a7a80
melcholign <115886450+melcholign@users.noreply.github.com>
2024-01-01 17:13:42 +0600

    Add error-handling code for playerSelection()
    if input is canceled or equal to empty string, null is returned. else
    for input not equal to any of the valid selections, an alert message is
    sent to inform of the error and then prompt for re-input

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ function getComputerChoice() {
 
 function playerSelection() {
 
-    let selection = prompt('Do you choose rock, paper, or scissor?');
-    return selection.toLowerCase();
+    let selection, status;
+
+    while (true) {
+
+        selection = prompt('Do you choose rock, paper, or scissor?');
+
+        if (selection === null) {
+            return null;
+        }
+
+        selection.trim();
+
+        if (selection === '') {
+            return null;
+        }
+
+        selection = selection.toLowerCase();
+
+        if (selection === 'rock' || selection === 'paper' || selection === 'scissor') {
+            return selection;
+        }
+
+        alert('Invalid choice. Try again.');
+    }
 }
